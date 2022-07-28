23ddfa59101989d6b21408d5048664f02d4e2218
Alex Castelo <alexcastelo437@gmail.com>
2022-07-27 21:04:25 -0300

    feat: importing style and creating main div

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import {FiSearch} from 'react-icons/fi'
+import './App.css'
 function App() {
   return (
     <div className="container">
-      <h1 className="title">Buscador de CEP</h1>
-     
-      <div className="containerInput">
-      <input type='text' 
-      placeholder="insira seu Cep..."
-      />
+        <h1 className="title">Buscador de CEP</h1>
+      
+        <div className="containerInput">
+          <input type='text' 
+          placeholder="insira seu Cep..."
+          />
 
-      <button className="buttonSearch">
-        <FiSearch size={25} color='#fff' />
-      </button>
+          <button className="buttonSearch">
+            <FiSearch size={25} color='#fff' />
+          </button>
+        </div>
+
+        <div className='main'>
+          <h2>CEP:</h2>
+
+          <span>Rua de teste</span>
+          <span>Complemento</span>
+          <span>vila </span>
+          <span>estado localizado - ur</span>
 
 
-      </div>
+
+        </div>
+
+
     </div>
   );
 }
